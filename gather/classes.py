7328e5a27a98a23deac72c9b499dcf9644ae8043
Kyllo Brooks <kyllobrooks@gmail.com>
2023-02-01 13:57:08 -0600

    pushed to tables

--- gather/classes.py
+++ gather/classes.py
@@ -6,7 +6,21 @@ from tqdm import tqdm
 import datetime
 import re
 import itertools
+import json
+import praw
+import sys
+import os
+import django
+import datetime
+import pytz
+import numpy
+import itertools
 
+sys.path.append('/home/kyllo/projects/gather_bot/gather')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+django.setup()
+
+from gather.models import Subreddit, Inference_task, Subreddit_result, Subreddit_mod, Comment_result
 
 # TODO: Need a class or function for the object describing the matrix of relationships between subs, maybe a sparse matrix
 # TODO: Need a class or function to handle pushing the data into a database
@@ -241,15 +255,18 @@ class Task_manager():
         self.__chunk_size = chunk_size
         self.__Gather_list = []
         self.__subreddit_list = []
-        self.__dims = len(self.__Gather_list)
+        # constructors
         self.__build_Gather_lists()
+        self.__dims = len(self.__Gather_list)
+        self.__edges_list = self.__wrap_JSON_edges()
+        self.__push_All()
     
     # defines a function to build a list of Gather objects
     def __build_Gather_lists(self):
         for sub in self.__subreddit_set:            
             sample = Gather(sub, self.__url, self.__apikey, self.__praw,    scope=self.__task.time_scale, 
                                                                             min_words=self.__task.min_words, 
-                                                                            inference=False, 
+                                                                            inference=True, 
                                                                             forest_width=self.__task.forest_width, 
                                                                             per_post_n=self.__task.per_post_n, 
                                                                             comments_n=self.__task.comments_n, 
@@ -258,28 +275,104 @@ class Task_manager():
             self.__Gather_list.append(sample)
             self.__subreddit_list.append(sample.name)
     
-    # defines a funtion to build the JSON edges object, JSON are just dicts of strings / ints
-    def __build_Matrix(self):
-        for gather in self.__Gather_list:
-            pass        
+    # this sucked to write i need a whiteboard to explain it
+    def __discover_JSON_edges(self, context):
+        outer = []
+
+        for i in range(self.__dims):
+            inner = {}
+            for j in range(i+1, self.__dims):
+                
+                if context == 'mods':
+                    A = self.__Gather_list[i].mod_set()
+                    B = self.__Gather_list[j].mod_set()
+                    intersect = A.intersection(B)
+                    cardinality = len(intersect)
+                    if cardinality > 0:
+                        data = { self.__Gather_list[j].name():cardinality}
+                        inner.update(data)
+                
+                if context == 'authors':
+                    A = self.__Gather_list[i].author_set()
+                    B = self.__Gather_list[j].author_set()
+                    intersect = A.intersection(B)
+                    cardinality = len(intersect)
+                    if cardinality > 0:
+                        data = {self.__Gather_list[j].name():cardinality}
+                        inner.update(data)
+            
+            outer.append(inner)
     
+        return outer
+
+    def __wrap_JSON_edges(self):
+        mods_edges = self.__discover_JSON_edges('mods')
+        authors_edges = self.__discover_JSON_edges('authors')
+        wrapper = []
+        for (m, a) in zip (mods_edges, authors_edges):
+            data = {"mods":m , "authors":a}
+            wrapper.append(data)
+        return wrapper
+    
+    def __push_Subreddit(self, sub_object):
+        custom_id  = sub_object.info()['pk']
+        display_name = sub_object.info()['display_name']
+        r = Subreddit(custom_id=custom_id, display_name=display_name)
+        r.save()
+        return r
+
+    def __push_Subreddit_result(self, sub_object, edges_json, subreddit, inference_task):
+        min = sub_object.stats()['min']
+        max = sub_object.stats()['max']
+        mean = sub_object.stats()['mean']
+        std = sub_object.stats()['std']
+        timestamp = sub_object.stats()['timestamp']
+        edges = edges_json
+        r = Subreddit_result(   subreddit=subreddit, 
+                                inference_task=inference_task, 
+                                min_result=min, 
+                                max_result=max, 
+                                mean_result=mean, 
+                                std_result=std, 
+                                timestamp=timestamp,
+                                edges=json.dumps(edges))
+        r.save()
+        return r
+
+    def __push_Subreddit_mod(self, sub_object, subreddit, result):
+        for mod in sub_object.mod_set():
+            r = Subreddit_mod(subreddit=subreddit, username=mod, subreddit_result=result)
+            r.save()
+
+    def __push_Comment_result(self, sub_object, subreddit, result):
+        for comment in sub_object.data():
+            subreddit_result = result
+            subreddit = subreddit
+            permalink = comment['permalink']
+            mhs_score = comment['mhs_score']
+            comment_body = comment['comment_body']
+            username = comment['username']
+            r = Comment_result( subreddit_result=result, 
+                                subreddit=subreddit, 
+                                permalink=permalink, 
+                                mhs_score=mhs_score, 
+                                comment_body=comment_body, 
+                                username=username )                              
+            r.save()
+
+    def __push_Gather(self, sub_object, edges, task):
+        s = self.__push_Subreddit(sub_object)
+        r = self.__push_Subreddit_result(sub_object, edges, s, task )
+        self.__push_Subreddit_mod(sub_object, s, r)
+        self.__push_Comment_result(sub_object, s, r )
+
+    def __push_All(self):
+        for (gather, edge) in zip(self.__Gather_list, self.__edges_list):
+            self.__push_Gather(gather, edge, self.__task)
+
     # accessor functions
     def gather_list(self):
         return self.__Gather_list
-
-
-
-# A few helper functions
-def jaccard_index(A, B):
-    """A function that returns the Jaccard similarity index for a given two sets:  
     
-    |AnB| / |AuB|
-    
-    A -- set - required, a python set object
-    B -- set - required, a python set object
-    """
-    return len( A.intersection(B) ) / len( A.union(B) )
-
-def print_dict(dictionary):
-    for key, value in dictionary.items():
-        print(key +' : '+ str(value))
+    def edges_list(self):
+        return self.__edges_list
--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,12 +74,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+# Allows you to toggle if you are using the cloud SQL proxy using an environment variable
+if os.getenv('DEVPROXY') == 'True':
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mhs-test',
+        'USER': 'django',
+        'PASSWORD': 'vC}DTT#q-4`H_|rU',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
-}
+    }   
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
